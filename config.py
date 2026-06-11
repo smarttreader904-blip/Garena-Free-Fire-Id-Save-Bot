b4c022d530af68697a2439630ee1e7ca908527ab
smarttreader904-blip <smarttreader904@gmail.com>
2026-06-11 12:01:02 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+# ================= BOT TOKEN =================
+BOT_TOKEN = "8859975301:AAEqR6ut9U_Vh77slUhAj5RdbwQmt2OoTXk"
+
+
+# ================= ADMINS =================
+# যাদের admin permission থাকবে (edit/delete সব access পাবে)
+ADMINS = [
+    8136997138,
+    8559227368
+]
+
+
+# ================= DATABASE FILE =================
+DB_NAME = "database.db"
+
+
+# ================= OPTIONAL SETTINGS =================
+BOT_NAME = "FF Save Bot"
